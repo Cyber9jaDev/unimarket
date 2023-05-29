06ecd32442c4c88f1a7a5a42d6fd498bf310ad13
Cyber9ja <ayodejioladapo96@gmail.com>
2023-05-29 15:20:29 +0100

    update

--- server.js
+++ server.js
@@ -44,12 +44,6 @@ app.use(express.json({limit: '50mb'}));
 //   });
 // }
 
-if (process.env.NODE_ENV === 'production') {
-  //*Set static folder up in production
-  app.use(express.static('client/build'));
-
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
 
 // morgan middleware, only runs in development mode
 if(process.env.NODE_ENV !== 'production'){ app.use(morgan('dev')) }
